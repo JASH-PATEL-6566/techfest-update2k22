ad2bf3af3c0d03bf2dff5d4d327d83dbe1a51603
Jash <100846378+JASH-PATEL-6566@users.noreply.github.com>
2022-09-22 11:29:57 +0530

    #3 no of card increase

--- src/Components/Auto-profileCard/AutoCard.js
+++ src/Components/Auto-profileCard/AutoCard.js
@@ -5,13 +5,14 @@ import React from 'react'
 import './autocard.css'
 
 function AutoCard() {
+    const number_of_cards = 6;
 
     var counter = 1;
 
     setInterval(function () {
         document.getElementById('radio' + counter).checked = true;
         counter++;
-        if (counter > 3) {
+        if (counter > number_of_cards) {
             counter = 1
         }
     }, 3000)
@@ -23,6 +24,9 @@ function AutoCard() {
                     <input type="radio" name="radio-btn" id="radio1" />
                     <input type="radio" name="radio-btn" id="radio2" />
                     <input type="radio" name="radio-btn" id="radio3" />
+                    <input type="radio" name="radio-btn" id="radio4" />
+                    <input type="radio" name="radio-btn" id="radio5" />
+                    <input type="radio" name="radio-btn" id="radio6" />
 
                     <div className="slide-auto first">
                         <div className="profile-card">
@@ -45,6 +49,27 @@ function AutoCard() {
                             <div className="profile-position">Web Designer</div>
                         </div>
                     </div>
+                    <div className="slide-auto">
+                        <div className="profile-card">
+                            <img src="https://i.imgur.com/bZBG9PE.jpg" alt="image1" className="profile-icon" />
+                            <div className="profile-name">Kelly Seikma 4</div>
+                            <div className="profile-position">Web Designer</div>
+                        </div>
+                    </div>
+                    <div className="slide-auto">
+                        <div className="profile-card">
+                            <img src="https://i.imgur.com/bZBG9PE.jpg" alt="image1" className="profile-icon" />
+                            <div className="profile-name">Kelly Seikma 5</div>
+                            <div className="profile-position">Web Designer</div>
+                        </div>
+                    </div>
+                    <div className="slide-auto">
+                        <div className="profile-card">
+                            <img src="https://i.imgur.com/bZBG9PE.jpg" alt="image1" className="profile-icon" />
+                            <div className="profile-name">Kelly Seikma 6</div>
+                            <div className="profile-position">Web Designer</div>
+                        </div>
+                    </div>
 
                     {/* <div className="slide-auto">
                         <img src={img2} alt="h" />
@@ -57,12 +82,18 @@ function AutoCard() {
                         <div className="auto-btn1"></div>
                         <div className="auto-btn2"></div>
                         <div className="auto-btn3"></div>
+                        <div className="auto-btn4"></div>
+                        <div className="auto-btn5"></div>
+                        <div className="auto-btn6"></div>
                     </div>
 
                     <div className="navigation-manual">
                         <label htmlFor="radio1" className='manual-btn'></label>
                         <label htmlFor="radio2" className='manual-btn'></label>
                         <label htmlFor="radio3" className='manual-btn'></label>
+                        <label htmlFor="radio4" className='manual-btn'></label>
+                        <label htmlFor="radio5" className='manual-btn'></label>
+                        <label htmlFor="radio6" className='manual-btn'></label>
                     </div>
                 </div>
             </div>
